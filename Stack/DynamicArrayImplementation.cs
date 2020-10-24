505fc3bdf46942849e9910817ec4a155d81c377d
vrallabandi <vrallabandi@dealersocket.com>
2020-10-24 19:35:40 +0530

    Stack using dynamic array implementation

---
+++ Stack/DynamicArrayImplementation.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Stack
+{
+    class DynamicArrayImplementation
+    {
+        private Stack stack = new Stack(1);
+
+        //Is Empty
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            return stack.Top == -1;
+        }
+
+        //Is Full
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        /// <returns></returns>
+        public bool IsFull()
+        {
+            return stack.Capacity == stack.Top + 1;
+        }
+
+        //Push
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        /// <param name="data"></param>
+        public void Push(int data)
+        {
+            if (IsFull())
+            {
+                DoubleArray();
+            }
+            stack.Array[++stack.Top] = data;
+        }
+
+        //Pop
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        public void Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Sorry!!! Stack is Empty...");
+                return;
+            }
+            Console.WriteLine("{0} is Poped from stack", stack.Array[stack.Top--]);
+        }
+
+        //Peek
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        public void Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Sorry!!! Stack is Empty...");
+                return;
+            }
+            Console.WriteLine("{0} is top element in stack", stack.Array[stack.Top]);
+        }
+
+        //Delete Stack
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        public void Delete()
+        {
+            stack = new Stack(stack.Capacity);
+        }
+
+        //Size
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        public void Size()
+        {
+            Console.WriteLine("Stack size is: {0}", stack.Top + 1);
+        }
+
+
+        /// <summary>
+        /// O(n) is time complexity 
+        /// </summary>
+        public void PrintStack()
+        {
+            if (IsEmpty())
+            {
+                return;
+            }
+
+            for (int i = 0; i <= stack.Top; i++)
+            {
+                Console.Write(stack.Array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// O(n) is time complexity 
+        /// </summary>
+        public void DoubleArray()
+        {
+            int[] tempArray= new int[stack.Capacity * 2];
+            for (int i = 0; i < stack.Capacity; i++)
+            {
+                tempArray[i] = stack.Array[i];
+            }
+
+            stack.Array = tempArray;
+            stack.Capacity *= 2;
+        }
+
+    }
+}
--- Stack/Program.cs
+++ Stack/Program.cs
@@ -6,7 +6,8 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            StackUsingSimpleArray();
+            //StackUsingSimpleArray();
+            StackUsingDynamicArray();
         }
 
         static void StackUsingSimpleArray()
@@ -47,7 +48,54 @@ namespace Stack
                         stack.Size();
                         break;
                 }
-                Console.WriteLine("Linked List Elements are: ");
+                Console.WriteLine("Stack Elements are: ");
+                stack.PrintStack();
+
+                Console.WriteLine("Press 0 to Exit");
+                ch = Console.ReadLine()[0];
+
+            } while (ch != '0');
+        }
+
+        static void StackUsingDynamicArray()
+        {
+            int choice;
+            char ch;
+            int data;
+            DynamicArrayImplementation stack = new DynamicArrayImplementation();
+            do
+            {
+
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Peek");
+                Console.WriteLine("4. Delete Stack");
+                Console.WriteLine("5. Size");
+
+                Console.WriteLine("<<< Enter Choice >>>");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter data");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        stack.Push(data);
+                        break;
+                    case 2:
+                        stack.Pop();
+                        break;
+                    case 3:
+                        stack.Peek();
+                        break;
+                    case 4:
+                        stack.Delete();
+                        break;
+                    case 5:
+                        stack.Size();
+                        break;
+                }
+                Console.WriteLine("Stack Elements are: ");
                 stack.PrintStack();
 
                 Console.WriteLine("Press 0 to Exit");
