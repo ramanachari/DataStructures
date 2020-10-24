101a0559973bba737e031cb07023985a2e11c492
vrallabandi <vrallabandi@dealersocket.com>
2020-10-24 19:23:23 +0530

    Stack using simple array

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -2,11 +2,59 @@
 
 namespace Stack
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StackUsingSimpleArray();
+        }
+
+        static void StackUsingSimpleArray()
+        {
+            int choice;
+            char ch;
+            int data;
+            StackUsingArray stack = new StackUsingArray();
+            do
+            {
+
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Peek");
+                Console.WriteLine("4. Delete Stack");
+                Console.WriteLine("5. Size");
+
+                Console.WriteLine("<<< Enter Choice >>>");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter data");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        stack.Push(data);
+                        break;
+                    case 2:
+                        stack.Pop();
+                        break;
+                    case 3:
+                        stack.Peek();
+                        break;
+                    case 4:
+                        stack.Delete();
+                        break;
+                    case 5:
+                        stack.Size();
+                        break;
+                }
+                Console.WriteLine("Linked List Elements are: ");
+                stack.PrintStack();
+
+                Console.WriteLine("Press 0 to Exit");
+                ch = Console.ReadLine()[0];
+
+            } while (ch != '0');
         }
     }
 }
+
---
+++ Stack/StackUsingArray.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace Stack
+{
+
+    class Stack
+    {
+        public int Capacity { get; set; }
+        public int Top { get; set; }
+        public int[] Array { get; set; }
+        public Stack(int capacity)
+        {
+            Capacity = capacity;
+            Top = -1;
+            Array = new int[capacity];
+        }
+    }
+
+    class StackUsingArray
+    {
+
+        private Stack stack = new Stack(5);
+
+        //Is Empty
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            return stack.Top == -1;
+        }
+
+        //Is Full
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        /// <returns></returns>
+        public bool IsFull()
+        {
+            return stack.Capacity == stack.Top + 1;
+        }
+
+        //Push
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        /// <param name="data"></param>
+        public void Push(int data)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Sorry!!! Stack is Full...");
+                return;
+            }
+            stack.Array[++stack.Top] = data;
+        }
+
+        //Pop
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        public void Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Sorry!!! Stack is Empty...");
+                return;
+            }
+            Console.WriteLine("{0} is Poped from stack", stack.Array[stack.Top--]);
+        }
+
+        //Peek
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        public void Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Sorry!!! Stack is Empty...");
+                return;
+            }
+            Console.WriteLine("{0} is top element in stack", stack.Array[stack.Top]);
+        }
+
+        //Delete Stack
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        public void Delete()
+        {
+            stack = new Stack(stack.Capacity);
+        }
+
+        //Size
+        /// <summary>
+        /// O(1) is time complexity 
+        /// </summary>
+        public void Size()
+        {
+            Console.WriteLine("Stack size is: {0}", stack.Top + 1);
+        }
+
+
+        /// <summary>
+        /// O(n) is time complexity 
+        /// </summary>
+        public void PrintStack()
+        {
+            if (IsEmpty())
+            {
+                return;
+            }
+            
+            for (int i = 0; i <= stack.Top; i++)
+            {
+                Console.Write(stack.Array[i]+" ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
