52c4726a51ffa2c47eab0a61ec11080afe9a3343
vrallabandi <vrallabandi@dealersocket.com>
2020-10-24 20:02:49 +0530

    Implementation of stack using LinkedList

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -7,7 +7,8 @@ namespace Stack
         static void Main(string[] args)
         {
             //StackUsingSimpleArray();
-            StackUsingDynamicArray();
+            //StackUsingDynamicArray();
+            StackUsingLinkedList();
         }
 
         static void StackUsingSimpleArray()
@@ -97,6 +98,53 @@ namespace Stack
                 }
                 Console.WriteLine("Stack Elements are: ");
                 stack.PrintStack();
+
+                Console.WriteLine("Press 0 to Exit");
+                ch = Console.ReadLine()[0];
+
+            } while (ch != '0');
+        }
+
+        static void StackUsingLinkedList()
+        {
+            int choice;
+            char ch;
+            int data;
+            StackUsingLinkedList stack = new StackUsingLinkedList();
+            do
+            {
+
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Peek");
+                Console.WriteLine("4. Delete Stack");
+                Console.WriteLine("5. Size");
+
+                Console.WriteLine("<<< Enter Choice >>>");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter data");
+                        data = Convert.ToInt32(Console.ReadLine());
+                        stack.Push(data);
+                        break;
+                    case 2:
+                        stack.Pop();
+                        break;
+                    case 3:
+                        stack.Peek();
+                        break;
+                    case 4:
+                        stack.Delete();
+                        break;
+                    case 5:
+                        stack.Size();
+                        break;
+                }
+                Console.WriteLine("Stack Elements are: ");
+                stack.Print();
 
                 Console.WriteLine("Press 0 to Exit");
                 ch = Console.ReadLine()[0];
---
+++ Stack/StackUsingLinkedList.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Stack
+{
+    class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }
+
+    class StackUsingLinkedList
+    {
+        public Node top = null;
+
+        //Is Empty
+        /// <summary>
+        /// O(1) is time complexity
+        /// </summary>
+        /// <returns></returns>
+        public bool IsEmpty()
+        {
+            if (top == null)
+                return true;
+            return false;
+        }
+
+        //Size
+        /// <summary>
+        /// O(n) is time complexity
+        /// </summary>
+        public void Size()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is Empty...");
+                return;
+            }
+            int count = 0;
+            Node curNode = top;
+            while (curNode != null)
+            {
+                count++;
+                curNode = curNode.Next;
+            }
+            Console.WriteLine("Stack size is: {0}",count);
+        }
+
+        //push
+        /// <summary>
+        /// O(1) is time complexity
+        /// </summary>
+        /// <param name="data"></param>
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            newNode.Next = top;
+            top = newNode;
+        }
+
+        //pop
+        /// <summary>
+        /// O(1) is time complexity
+        /// </summary>
+        public void Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Sorry!!! Stack is Empty...");
+                return;
+            }
+            Console.WriteLine("{0} is poped from stack", top.Data);
+            top = top.Next;
+        }
+
+        //Delete
+        /// <summary>
+        /// O(1) is time complexity
+        /// </summary>
+        public void Delete()
+        {
+            top = null;
+        }
+
+        //Peek
+        /// <summary>
+        /// O(1) is time complexity
+        /// </summary>
+        public void Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Sorry!!! Stack is Empty...");
+                return;
+            }
+            Console.WriteLine("{0} is top element in stack", top.Data);
+        }
+
+        //Print elements
+        /// <summary>
+        /// O(n) is time complexity
+        /// </summary>
+        public void Print()
+        {
+            Node curNode = top;
+            while (curNode != null)
+            {
+                Console.Write(curNode.Data + " ");
+                curNode = curNode.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
